45bc72addd5e97b5b60de951a5bfef400182579d
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2015-10-14 11:52:26 -0400

    Fixed formatting and added display test

---
+++ disp_test.py
@@ -0,0 +1,39 @@
+'''
+Display test for PyschoPy.  We will take a video of the screen as it 
+displays this test, then we will attempt to extract the screen from
+the video.
+'''
+
+from psychopy import visual, core
+
+
+def main():
+    testWin = visual.Window(
+            size=(1280, 800), monitor="tobiiMonitor", units="pix", screen=0,
+            fullscr=True)
+    x = -0.5
+    y = -0.5
+    dotStim = visual.Circle(testWin, radius=0.05, fillColor=[0, 1, 0],
+                            units='norm', pos=(x, y), autoDraw=True)
+    testWin.flip()
+    core.wait(4)
+    for i in range(100):
+        x = x+0.01
+        dotStim.pos = (x, y)
+        testWin.flip()
+    for i in range(100):
+        y = y+0.01
+        dotStim.pos = (x, y)
+        testWin.flip()
+    for i in range(100):
+        x = x-0.01
+        dotStim.pos = (x, y)
+        testWin.flip()
+    for i in range(100):
+        y = y-0.01
+        dotStim.pos = (x, y)
+        testWin.flip()
+    testWin.close()
+
+if __name__ == '__main__':
+    main()
--- find_screen_utils.py
+++ find_screen_utils.py
@@ -1,3 +1,7 @@
+'''
+Utilities to find monitor/screen coordinates within an image.
+Used for eye tracking studies.
+'''
 import cv2
 import numpy as np
 
@@ -9,12 +13,12 @@ def group_lines(lines):
     horz = []
     vert = []
     # split into horizontal and vertical lines
-    for x1,y1,x2,y2 in lines:
+    for x1, y1, x2, y2 in lines:
         # if change in x is less than in y, then classify as vertical
         if np.abs(x1-x2) < np.abs(y1-y2):
-            vert.append((x1,y1,x2,y2))
+            vert.append((x1, y1, x2, y2))
         else:
-            horz.append((x1,y1,x2,y2))
+            horz.append((x1, y1, x2, y2)) 
 
     # find (very) approximate center of edges
     horz = np.array(horz)
@@ -25,7 +29,7 @@ def group_lines(lines):
     # split horizontal lines into top and bottom
     top = []
     bottom = []
-    for x1,y1,x2,y2 in horz:
+    for x1, y1, x2, y2 in horz:
         if y1 < y_center:
             top.append((x1, y1))
             top.append((x2, y2))
@@ -38,7 +42,7 @@ def group_lines(lines):
     # split vertical lines into left and right
     left = []
     right = []
-    for x1,y1,x2,y2 in vert:
+    for x1, y1, x2, y2 in vert:
         if x1 > x_center:
             right.append((x1, y1))
             right.append((x2, y2))
@@ -49,7 +53,8 @@ def group_lines(lines):
     right = np.array(right)
 
     # return points that consist each edge of the screen
-    return ((top, bottom) , (left, right))
+    return ((top, bottom), (left, right))
+
 
 def fit_edges(groups):
     '''
@@ -61,17 +66,17 @@ def fit_edges(groups):
     h_fits = []
     v_fits = []
     for group in horz:
-        h_fits.append(np.polyfit(group[:,0], group[:,1], 1)) # fit line as function of x values
+        h_fits.append(np.polyfit(group[:, 0], group[:, 1], 1))  # fit line as function of x values
     for group in vert:
-        v_fits.append(np.polyfit(group[:,1], group[:,0], 1)) # fit line as function of y values
+        v_fits.append(np.polyfit(group[:, 1], group[:, 0], 1))  # fit line as function of y values
 
     return h_fits, v_fits
+
 
 def get_lines(img, coefs):
     '''
     Takes linear fits and gets endpoints within the image
     '''
-    plt_img = np.array(img)
     horz_coefs = coefs[0]
     vert_coefs = coefs[1]
 
@@ -80,29 +85,31 @@ def get_lines(img, coefs):
     # get endpoints for horz lines
     for coef in horz_coefs:
         x1 = 0
-        x2 = len(img[0,:]) - 1
+        x2 = len(img[0, :]) - 1
         y1 = int(coef[1] + x1*coef[0])
         y2 = int(coef[1] + x2*coef[0])
-        lines.append((x1,y1,x2,y2))
+        lines.append((x1, y1, x2, y2))
 
     # get endpoints for vert lines
     for coef in vert_coefs:
         y1 = 0
-        y2 = len(img[:,0]) - 1
+        y2 = len(img[:, 0]) - 1
         x1 = int(coef[1] + y1*coef[0])
         x2 = int(coef[1] + y2*coef[0])
-        lines.append((x1,y1,x2,y2))
+        lines.append((x1, y1, x2, y2))
 
     return lines
+
 
 def find_corners(img, lines):
     '''
     Find corners of screen within image based on edge lines.
     '''
-    max_x = len(img[0,:]) - 1
-    max_y = len(img[:,1]) - 1
+    max_x = len(img[0, :]) - 1
+    max_y = len(img[:, 1]) - 1
 
     corners = []
+
     def intersects(l1, l2):
         '''
         Return point of intersection between two lines.
@@ -111,10 +118,10 @@ def find_corners(img, lines):
         x1, y1, x2, y2 = l1
         x3, y3, x4, y4 = l2
 
-        divisor = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
-        if divisor != 0:
-            pt_x = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / divisor
-            pt_y = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / divisor
+        d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+        if d != 0:
+            pt_x = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / d
+            pt_y = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / d
             if pt_x >= 0 and pt_x <= max_x and pt_y >= 0 and pt_y <= max_y:
                 return (pt_x, pt_y)
             else:
@@ -127,6 +134,7 @@ def find_corners(img, lines):
             if corner:
                 corners.append(corner)
     return corners
+
 
 def sort_corners(corners):
     '''
@@ -144,4 +152,3 @@ def sort_corners(corners):
         elif x < ctr_x and y > ctr_y:
             srtd_corners[3] = (x, y)
     return srtd_corners
-
