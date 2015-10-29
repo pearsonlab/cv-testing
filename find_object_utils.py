d54d564f96198bc3deb51a6bf3a2aa01473a445d
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2015-10-29 14:41:45 -0400

    Object finding with still image gaze plotting

--- find_object_utils.py
+++ find_object_utils.py
@@ -4,6 +4,9 @@ import numpy as np
 
 def drawMatches(img1, kp1, img2, kp2, matches, mask):
     """
+    Found this function on StackOverflow. Below is the original
+    description.
+
     My own implementation of cv2.drawMatches as OpenCV 2.4.9
     does not have this function available but it's supported in
     OpenCV 3.0.0
@@ -71,3 +74,42 @@ def drawMatches(img1, kp1, img2, kp2, matches, mask):
 
     # Also return the image if you'd like a copy
     return out
+
+
+def object_find(img1, img2, MIN_MATCH_COUNT):
+    '''
+    Finds an object (from still image, img1) within a frame (img2).  If
+    found it returns the transformation matrix from frame to still.
+
+    This code is based on the OpenCV feature detection tutorial
+    '''
+
+    sift = cv2.SIFT()
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    good = []
+    for m, n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        return M
+
+    else:
+        return None
+
+
+# def frame_to_still_gaze(gp_x, gp_y, M):
+
